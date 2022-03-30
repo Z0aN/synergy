5ffe1c4fb8d72c297d3cdf64335f6ae3dcc79dcf
ZoaN <adrinal1ne30rus@mail.ru>
2022-03-30 12:03:58 +0400

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/welcome.css">
+    <title>Главная</title>
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="header__logo">
+                <img src="../img/logo.svg" alt="Icon: Logo" class="header__logo--image">
+            </div>
+            <ul class="navbar-list">
+                <li class="navbar-list__item"><a href="#" class="navbar-list__link">Главная</a></li>
+                <li class="navbar-list__item"><a href="#" class="navbar-list__link">Авторизация</a></li>
+                <li class="navbar-list__item"><a href="#" class="navbar-list__link">Регистрация</a></li>
+                <li class="navbar-list__item"><a href="#" class="navbar-list__link">Личный кабинет</a></li>
+            </ul>
+        </header>
+    </div>
+    <div class="main">
+        <div class="container">
+            <span class="bg-tittle">Главная</span>
+            <div class="cards">
+                <div class="card">
+                    <div class="card__image"><img src="../img/image-1.jpg" alt="Picture" class="card__image--img"></div>
+                    <div class="card-subtittle"><span class="card-subtitle__text">Филология</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
